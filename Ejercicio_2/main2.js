1568ddcf3ee0b029a0da3d645c69674372088ff3
d-tiguila <dtiguila@centro.edu.mx>
2025-03-05 17:19:44 -0600

    modigying ejercicio 2

--- Ejercicio_2/main2.js
+++ Ejercicio_2/main2.js
@@ -7,26 +7,30 @@ const ctx = canvas.getContext('2d');
 
 
 let linesNum = Math.max(canvas.height, canvas.width);
-let C = linesNum*2 ; 
-let k = 8;  
+let C = linesNum * 2;
+let k = 120;
 
 for (let step = 0; step < linesNum; step++) {
-// funcion crecimiento exponencial :)     
-let factor = (Math.exp((step / linesNum) * k) - 1) / (1 + (step / linesNum) * (Math.exp(k) - 1));
-    let spacing = C * factor;  
-console.log(spacing);
+    //Funcion Exponencia para distancia entra lineas :)
+    let normalizedStep = step / linesNum;
+    let factor = (Math.exp(normalizedStep * k) - 1) / (Math.exp(k) - 1);
+    factor = (normalizedStep + factor) / 2.;
+
+    let spacing = C * factor;
+    console.log(spacing);
 
     ctx.beginPath();
     ctx.strokeStyle = "black";
     ctx.lineWidth = 3;
-    ctx.moveTo(0, spacing);  
+    ctx.moveTo(0, spacing);
     ctx.lineTo(spacing, 0);
     ctx.stroke();
 }
 
 
 
+
 ctx.beginPath();
-ctx.ellipse(canvas.width*.39, canvas.height*0.9, canvas.height*.4, canvas.height*.4, -Math.PI / 4, Math.PI, 2 * Math.PI);
+ctx.ellipse(canvas.width*.5, canvas.height*0.9, canvas.height*.4, canvas.height*.4, -Math.PI / 4, Math.PI, 2 * Math.PI);
 ctx.fillStyle = "#FF5349";
 ctx.fill();
